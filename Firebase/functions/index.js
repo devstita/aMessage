0c921d56ee353ca710e678063536f3b9fcf6cd70
Devstita <devstita@gmail.com>
2018-10-07 00:23:17 +0900

    DEVELOPING: Develop Keeping HTTP (or Socket) Connection (Session) Algorithm (with https://firebase.google.com/docs/functions/networking)

--- Firebase/functions/index.js
+++ Firebase/functions/index.js
@@ -1,9 +1,26 @@
+// DEVELOPING: Develop Keeping HTTP (or Socket) Connection (Session) Algorithm (with https://firebase.google.com/docs/functions/networking)
+
+const http = require('http');
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
-admin.initializeApp();
+const agent = new http.Agent({keepAlive: true});
 
-exports.checkNetwork = functions.https.onRequest((req, res) => {
-    let text = req.get('text');
-    res.send(text);
+exports.ForeverHTTPConnection = functions.https.onRequest((request, response) => {
+    const req = http.request({
+        host: '< HOST_ADDRESS >',
+        port: 80,
+        path: '< PATH >',
+        method: 'GET',
+        agent: agent, // ☆ Core Point ☆
+    }, (res) => {
+        let rawData = '';
+        res.setEncoding('utf8');
+        res.on('data', (chunk => { rawData += chunk }));
+        res.on('end', () => {
+            response.status(200).send(`Data: ${rawData}`);
+        });
+    });
+    req.on('error', e => {
+        response.status(500).send(`Error: ${e.message}`);
+    })
 });
