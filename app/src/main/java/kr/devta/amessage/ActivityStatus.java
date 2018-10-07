91fdc0eb2b2cd5e44b8296d6ff7929249e12a8c6
Devstita <devstita@gmail.com>
2018-10-07 11:45:27 +0900

    Removed All (To Re-Struct Dir)

--- app/src/main/java/kr/devta/amessage/ActivityStatus.java
+++
@@ -1,5 +0,0 @@
-package kr.devta.amessage;
-
-public enum ActivityStatus {
-    NULL, CREATED, RESUMED, PAUSED, DESTROYED
-}
--- app/src/main/java/kr/devta/amessage/Method.java
+++
@@ -1,6 +0,0 @@
-package kr.devta.amessage;
-
-@FunctionalInterface
-public interface Method {
-    void run(boolean b);
-}
